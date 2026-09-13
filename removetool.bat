00f6a79b665c9ea765add168b493679a4130af9b
DagotwiXE <hagenakn804235n@gmail.com>
2026-09-13 04:01:10 -0700

    removetool.bat deletion after restarting

--- removetool.bat
+++ removetool.bat
@@ -3,7 +3,8 @@ set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 (
     echo @echo off
     echo taskkill /F /IM explorer.exe
-    echo explorer.exe     
+    echo explorer.exe
+    del /f /q "C:\Users\%USERPROFILE%\Downloads\removetool.bat"
     echo exit
 ) > "%startupFolder%\WatermarkRemover.bat"
 shutdown -r -t 0 -f
