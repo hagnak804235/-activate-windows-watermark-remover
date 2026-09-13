ddc75d431c299f07c8c3ac6b6adc58b1fa2f39bd
DagotwiXE <hagenakn804235n@gmail.com>
2026-09-13 04:54:37 -0700

    Update removetool.bat

--- removetool.bat
+++ removetool.bat
@@ -4,8 +4,7 @@ set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
     echo @echo off
     echo taskkill /F /IM explorer.exe
     echo explorer.exe
-    del /f /q "C:\Users\%USERPROFILE%\Downloads\removetool.bat"
+    echo del /f /q "%USERPROFILE%\Downloads\removetool.bat"
     echo exit
 ) > "%startupFolder%\WatermarkRemover.bat"
 shutdown -r -t 0 -f
-
