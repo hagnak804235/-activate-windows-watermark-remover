36600b14cf60cd6b5c41ac61fa3ee2f1cf4442a5
DagotwiXE <hagenakn804235n@gmail.com>
2026-09-13 05:08:31 -0700

    Update removetool.bat

--- removetool.bat
+++ removetool.bat
@@ -2,6 +2,5 @@ set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 (
     echo taskkill /F /IM explorer.exe
     echo explorer.exe
-    echo exit
 ) > "%startupFolder%\WatermarkRemover.bat"
 shutdown -r -t 0 -f
